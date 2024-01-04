d95c07b7271996c1a18f73bab6aa79661a9f45d0
Irina Saveleva <goyalion17@gmail.com>
2024-01-04 19:08:30 +0100

    children Prop, reusable peace of code, parent lift, useState, react.fragment, controlled inputs, selects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [sum, setSum] = useState("");
+  const [percentTip, setPercentTip] = useState(0);
+  const [friendPercentTip, setFriendPercentTip] = useState(0);
+
+  const tip = (sum * (percentTip + friendPercentTip)) / 2 / 100;
+
+  function handleClearAll() {
+    setSum("");
+    setPercentTip(0);
+    setFriendPercentTip(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Rating text="How much was the bill?">
+        <input
+          type="text"
+          value={sum}
+          onChange={(e) => setSum(Number(e.target.value))}
+        />
+      </Rating>
+
+      <Rating text="How do you like the service?">
+        <SelectInput percentTip={percentTip} onPercentTip={setPercentTip} />
+      </Rating>
+
+      <Rating text="How did your friend like the service?">
+        <SelectInput
+          percentTip={friendPercentTip}
+          onPercentTip={setFriendPercentTip}
+        />
+      </Rating>
+
+      {sum > 0 && (
+        <>
+          <h3>
+            You pay ${sum + tip} ($
+            {sum} + ${tip} tip)
+          </h3>
+
+          <button onClick={handleClearAll}>Reset</button>
+        </>
+      )}
     </div>
   );
 }
 
-export default App;
+function SelectInput({ percentTip, onPercentTip }) {
+  return (
+    <select
+      value={percentTip}
+      onChange={(e) => onPercentTip(Number(e.target.value))}
+    >
+      <option value="0">Dissatisfied (0%)</option>
+      <option value="5">It was okay (5%)</option>
+      <option value="10">It was good (10%)</option>
+      <option value="20">Absolutely amazing! (20%)</option>
+    </select>
+  );
+}
+
+function Rating({ text, children }) {
+  return (
+    <div>
+      <p>{text}</p>
+      {children}
+    </div>
+  );
+}
